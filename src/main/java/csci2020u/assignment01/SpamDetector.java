48f2a5225cd7f78e75c277f7ee1a2a21ee3548b2
christopherFern <128930968+christopherFern@users.noreply.github.com>
2025-02-26 19:53:45 -0500

    Counting the frequency of words in the ham file no comments yet

--- src/main/java/csci2020u/assignment01/SpamDetector.java
+++ src/main/java/csci2020u/assignment01/SpamDetector.java
@@ -1,7 +1,62 @@
 package csci2020u.assignment01;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
 public class SpamDetector {
+
     public SpamDetector() {
         // TODO: Put your File I/O code and spam detection algorithm here
+        File hamFolder = new File(Objects.requireNonNull(getClass().getResource("/data/train/ham")).getFile());
+        File spamFolder = new File(Objects.requireNonNull(getClass().getResource("/data/train/spam")).getFile());
+
+        File[] hamFiles = hamFolder.listFiles();
+        File[] spamFiles = spamFolder.listFiles();
+
+        HashMap<String, Integer> trainHamFreq = new HashMap<>();
+        HashMap<String, Integer> trainSpamFreq = new HashMap<>();
+
+
+
+        if (spamFiles != null && hamFiles != null) {
+
+            for (int i = 0; i < hamFiles.length; i++) {
+                Set<String> wordsInCurrentFile = new HashSet<>();
+                try {
+                    FileReader fileReader = new FileReader(hamFiles[i]);
+                    BufferedReader bufferedReader = new BufferedReader(fileReader);
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        for (String word : line.split("\\s+")) {
+                            word = word.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+                            if (!word.isEmpty()) {
+                                if(!wordsInCurrentFile.contains(word)) {
+                                    wordsInCurrentFile.add(word);
+                                    if (trainHamFreq.containsKey(word)) {
+                                        trainHamFreq.put(word, trainHamFreq.get(word) + 1);
+                                    } else {
+                                        trainHamFreq.put(word, 1);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+            System.out.println(trainHamFreq);
+        }
+    }
+
+    public static void main(String[] args) {
+        new SpamDetector();
     }
 }
--- src/main/java/csci2020u/assignment01/SpamDetectorGUI.java
+++ src/main/java/csci2020u/assignment01/SpamDetectorGUI.java
@@ -3,5 +3,6 @@ package csci2020u.assignment01;
 public class SpamDetectorGUI {
     public static void main(String[] args) {
         // TODO: Put your Java Swing components here and you will use SpamDetector.java here
+
     }
 }
